ed04c650b6c79aefc0a430a2983e08a02eaf930b
hashrock <hashedrock@gmail.com>
2017-03-28 13:31:03 +0900

    fix(audio): remove click noise

--- src/audio.ts
+++ src/audio.ts
@@ -4,12 +4,18 @@ import * as util from "./util"
 export function playNote(noteNumber: number, length: number){
   var osc1 = audioContext.createOscillator();
   var amp = audioContext.createGain();
+  var release = 0.05;
   osc1.frequency.value = util.mtof(noteNumber);
   osc1.connect(amp);
   amp.gain.value = 0.1;
   osc1.start();
   amp.connect(audioContext.destination);
   setTimeout(function () {
-    osc1.disconnect();
+    var now = audioContext.currentTime;
+    console.log(now)
+    amp.gain.setValueAtTime(amp.gain.value, now);
+    amp.gain.linearRampToValueAtTime(0, now + release);
+    //osc1.disconnect();
+    osc1.stop(now + release);
   }, length);
 }
