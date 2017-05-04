f78cc7b8d6fde6e2992ae41e0f187018703ca4f1
hashrock <hashedrock@gmail.com>
2017-05-04 20:23:05 +0900

    feat(main): SMF export

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import {PianoRoll} from "./pianoroll"
 import * as mml from "./mml"
+import * as mml2smf from "mml2smf"
 
 var el: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector(".canvas");
 var piano: PianoRoll = new PianoRoll({
@@ -32,6 +33,35 @@ document.querySelector("#export-mml").addEventListener("click", ()=>{
   exportSource.value = mml.jsonToMML(piano.notes).join(";\n")
 })
 
+document.querySelector("#export-smf").addEventListener("click", ()=>{
+  //現在、ch1のみ。ボリューム指定は0-15を0-127に変換する必要があるが、対数にすべきか不明なのでとりあえず固定値にした
+  let binary = mml2smf(mml.jsonToMML(piano.notes).map(line=>"C1"+line).join(";\n").replace(/v10/g, "v80"))
+  downloadBlob(binary, 'minroll.mid', 'application/octet-stream');
+})
+
+function downloadBlob(data : Uint8Array, fileName: string, mimeType :string) {
+  let blob = new Blob([data], {
+    type: mimeType
+  });
+  let url = window.URL.createObjectURL(blob);
+  downloadURL(url, fileName);
+  setTimeout(function() {
+    return window.URL.revokeObjectURL(url);
+  }, 1000);
+};
+
+function downloadURL(data:string, fileName:string) {
+  var a;
+  a = document.createElement('a');
+  a.href = data;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.style.display = 'none';
+  a.click();
+  a.remove();
+};
+
+//downloadBlob(myBinaryBlob, 'some-file.bin', 'application/octet-stream');
 
 document.addEventListener("keypress", (e)=>{
   if(e.keyCode === 32){
