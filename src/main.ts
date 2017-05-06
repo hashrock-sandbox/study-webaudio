9e946e2ea2667f78f56b39dec825799590aaba29
hashrock <hashedrock@gmail.com>
2017-05-06 23:15:33 +0900

    refactor(download): export function

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ declare var Vue:any
 
 import {PianoRoll} from "./pianoroll"
 import * as mml from "./mml"
+import * as download from "./download"
 const mml2smf = require("mml2smf")
+
+
 
 var piano: PianoRoll
 
@@ -18,28 +21,6 @@ function togglePlaying(){
     playing = true
   }
 }
-
-function downloadBlob(data : Uint8Array, fileName: string, mimeType :string) {
-  let blob = new Blob([data], {
-    type: mimeType
-  });
-  let url = window.URL.createObjectURL(blob);
-  downloadURL(url, fileName);
-  setTimeout(function() {
-    return window.URL.revokeObjectURL(url);
-  }, 1000);
-};
-
-function downloadURL(data:string, fileName:string) {
-  var a;
-  a = document.createElement('a');
-  a.href = data;
-  a.download = fileName;
-  document.body.appendChild(a);
-  a.style.display = 'none';
-  a.click();
-  a.remove();
-};
 
 document.addEventListener("keypress", (e)=>{
   if(e.keyCode === 32){
@@ -80,7 +61,7 @@ new Vue({
     exportSmf: function(){
       //現在、ch1のみ。ボリューム指定は0-15を0-127に変換する必要があるが、対数にすべきか不明なのでとりあえず固定値にした
       let binary = mml2smf(mml.jsonToMML(piano.notes).map(line=>"C1"+line).join(";\n").replace(/v10/g, "v80"))
-      downloadBlob(binary, 'minroll.mid', 'application/octet-stream');
+      download.downloadBlob(binary, 'minroll.mid', 'application/octet-stream');
     },
     play: function(){
       togglePlaying()
